b60196cf79d05afcaa566b1c8b3bafde3530e287
KotiasFeet <m.kotia678@gmail.com>
2025-09-06 19:33:17 +0300

    Add project files.

---
+++ Motia_Dayn/Motia_Dayn.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+
+int da() {
+	std::cout << "da\n";
+	return 69;
+}
+
+int main()
+{
+
+	int N;
+	std::cin >> N;
+	
+
+	std::vector<std::vector<int>> matrix = { {} };
+
+	for (int i = 0; i < N; ++i) {
+		std::cout << i << "\n";
+	}
+    
+}
+
+
